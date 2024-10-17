299ce8bda83d9d99823fdf485b292d74e99cc4f9
Fernanda Vieira <vieiras.ferndanda@gmail.com>
2024-10-16 22:58:17 -0400

    Teste vercel

--- Services/MusicaService.cs
+++ Services/MusicaService.cs
@@ -7,16 +7,18 @@ namespace TriviaMusical.Services
     public class MusicaService
     {
         private readonly string _connectionString;
+        private List<Musica> _listaMusicasTemporaria = new List<Musica>(); // Lista temporária de músicas
+        private Random _random = new Random();
 
         public MusicaService(IConfiguration connectionString)
         {
             _connectionString = connectionString.GetConnectionString("TriviaMusicalDB");
+            CarregarMusicas();
         }
 
-        public Musica ObterMusicaAleatoria()
+        public void CarregarMusicas()
         {
-            string query = "SELECT TOP 1 IdMusica, NomeDoCantor, AnoLancamento, NomeDaMusica FROM Musicas ORDER BY NEWID()";
-            Musica musica = new Musica();
+            string query = "SELECT IdMusica, NomeDoCantor, AnoLancamento, NomeDaMusica FROM Musicas";
 
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
@@ -24,16 +26,37 @@ namespace TriviaMusical.Services
                 connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
-                if (reader.Read())
+                while (reader.Read())
                 {
-                    musica.IdMusica = reader["IdMusica"].ToString();
-                    musica.NomeDoCantor = reader["NomeDoCantor"].ToString();
-                    musica.AnoLancamento = Convert.ToInt32(reader["AnoLancamento"]);
-                    musica.NomeMusica = reader["NomeDaMusica"].ToString();
+                    Musica musica = new Musica
+                    {
+                        IdMusica = reader["IdMusica"].ToString(),
+                        NomeDoCantor = reader["NomeDoCantor"].ToString(),
+                        AnoLancamento = Convert.ToInt32(reader["AnoLancamento"]),
+                        NomeMusica = reader["NomeDaMusica"].ToString()
+                    };
+
+                    _listaMusicasTemporaria.Add(musica); // Adiciona cada música à lista temporária
                 }
             }
+        }
+        public Musica ObterMusicaAleatoria()
+        {
+            if (_listaMusicasTemporaria.Count == 0)
+            {
+                return null; // Retorna null se não houver mais músicas disponíveis
+            }
 
-            return musica;
+            int indexAleatorio = _random.Next(_listaMusicasTemporaria.Count); // Seleciona um índice aleatório
+            Musica musicaSelecionada = _listaMusicasTemporaria[indexAleatorio]; // Obtém a música selecionada
+
+            _listaMusicasTemporaria.RemoveAt(indexAleatorio); // Remove a música da lista temporária para evitar repetição
+
+            return musicaSelecionada; // Retorna a música selecionada
+        }
+        public List<Musica> ObterMusicas()
+        {
+            return _listaMusicasTemporaria;
         }
     }
 }
